0cd83001d155fcb450a9ce4805504639a2fa9dfb
Nilrajsinh <Nilrajsinh1@icloud.com>
2020-01-02 22:30:38 +0530

    Initial Commit

---
+++ Collage PortalTests/Collage_PortalTests.swift
@@ -0,0 +1,34 @@
+//
+//  Collage_PortalTests.swift
+//  Collage PortalTests
+//
+//  Created by Nilrajsinh Vaghela on 02/01/20.
+//  Copyright © 2020 Nilrajsinh Vaghela. All rights reserved.
+//
+
+import XCTest
+@testable import Collage_Portal
+
+class Collage_PortalTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
